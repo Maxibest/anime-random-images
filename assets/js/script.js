8fcf5c9c8526ea445ba145af9da0a31e0070c914
Kaïto <yohann.gonthier.jr@gmail.com>
2025-08-15 15:25:50 +0200

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,11 @@
 const anime_img = document.getElementById("img");
 
 btn.addEventListener("click", () => {
-    fetch("https://api.nekosapi.com/v3/images/random?limit=1")
-    .then(res => res.json())
+    fetch("https://nekos.best/api/v2/hug?amount=1", {
+        method: "GET",
+    }).then(res => res.json())
     .then(data => {
-        const imageUrl = data.items[0].image_url;
+        const imageUrl = data.results[0].url;
         anime_img.src = imageUrl;
     })
     .catch(error => console.error('Erreur lors de la récupération des données :', error));
